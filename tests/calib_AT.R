2ea49d4de584babf1453eae88460ab1518ee28f2
Johannes Gussenbauer - QM <gussenbauer@xlwt0006.statistik.local>
2018-03-05 09:05:24 +0100

    sensitivity analysis and batch scripts upbdated

--- tests/calib_AT.R
+++ tests/calib_AT.R
@@ -2,8 +2,9 @@
 # cluster schätzen
 library(data.table)
 library(surveysd)
+library(mountSTAT)
 
-dat <- fread("/mnt/obdatenaustausch/NETSILC3/udb_short_new.csv")
+dat <- fread(paste0(mountO(),"/B/Datenaustausch/NETSILC3/udb_short_new.csv"))
 dat[,RB050:=gsub(",","\\.",RB050)]
 dat[,RB050:=as.numeric(RB050)]
 
@@ -15,10 +16,10 @@ set.seed(1234)
 dat_boot <- draw.bootstrap(dat=copy(dat_at),REP=1000,hid="DB030",weights="RB050",strata="DB040",
                            year="RB010",split=TRUE,pid="RB030")
 
-write.csv2(dat_boot,file="/mnt/obdatenaustausch/NETSILC3/udb_AT_bootweight_1000.csv")
+# write.csv2(dat_boot,file="/mnt/obdatenaustausch/NETSILC3/udb_AT_bootweight_250.csv")
 
 dat_boot_calib <- recalib(dat=copy(dat_boot),hid="DB030",weights="RB050",
                           year="RB010",b.rep=paste0("w",1:1000),conP.var=c("RB090"),conH.var = c("DB040"))
 
-write.csv2(dat_boot_calib,file="/mnt/obdatenaustausch/NETSILC3/udb_AT_calib_1000.csv")
+fwrite(dat_boot_calib,file=paste0(mountO(),"/B/Datenaustausch/NETSILC3/udb_AT_calib.csv"))
 
