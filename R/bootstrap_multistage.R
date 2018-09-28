2106da4101828c321d2203373eff765290df4564
Gregor de Cillia <de.cillia.gregor@gmail.com>
2018-09-28 17:57:18 +0200

    document rescaled.bootstrap and recalib with markdown

--- R/bootstrap_multistage.R
+++ R/bootstrap_multistage.R
@@ -5,26 +5,26 @@
 #'
 #' @param dat either data frame or data table containing the survey sample
 #' @param REP integer indicating the number of bootstraps to be drawn
-#' @param strata string specifying the column name in \code{dat} that is used for stratification. For multistage sampling multiple column names can be specified by \code{strata=c("strata1>strata2>strata3")}.
+#' @param strata string specifying the column name in `dat` that is used for stratification. For multistage sampling multiple column names can be specified by `strata=c("strata1>strata2>strata3")`.
 #' See Details for more information.
-#' @param cluster string specifying the column name in \code{dat} that is used for clustering. For multistage sampling multiple column names can be specified by \code{cluster=c("cluster1>cluster2>cluster3")}.
+#' @param cluster string specifying the column name in `dat` that is used for clustering. For multistage sampling multiple column names can be specified by `cluster=c("cluster1>cluster2>cluster3")`.
 #' See Details for more information.
-#' @param fpc string specifying the column name in \code{dat} that contains the number of PSUs at the first stage. For multistage sampling the number of PSUs at each stage must be specified by \code{strata=c("fpc1>fpc2>fpc3")}.
+#' @param fpc string specifying the column name in `dat` that contains the number of PSUs at the first stage. For multistage sampling the number of PSUs at each stage must be specified by `strata=c("fpc1>fpc2>fpc3")`.
 #' @param single.PSU either "merge" or "mean" defining how single PSUs need to be dealt with.
-#' For \code{single.PSU="merge"} single PSUs at each stage are merged with the strata or cluster with the next least number of PSUs. If multiple of those exist one will be select via random draw
-#' For \code{single.PSU="mean"} single PSUs will get the mean over all bootstrap replicates at the stage which did not contain single PSUs.
-#' @param return.value either "data" or "replicates" specifying the return value of the function. For "data" the survey data is returned as class \code{data.table}, for "replicates" only the bootstrap replicates are returned as \code{data.table}.
+#' For `single.PSU="merge"` single PSUs at each stage are merged with the strata or cluster with the next least number of PSUs. If multiple of those exist one will be select via random draw
+#' For `single.PSU="mean"` single PSUs will get the mean over all bootstrap replicates at the stage which did not contain single PSUs.
+#' @param return.value either "data" or "replicates" specifying the return value of the function. For "data" the survey data is returned as class `data.table`, for "replicates" only the bootstrap replicates are returned as `data.table`.
 #' @param check.input logical, if TRUE the input will be checked before applying the bootstrap procedure
-#' @param new.method logical, if TRUE bootstrap replicates will never be negative even if in some strata the whole population is in the sample. WARNING: This is still experimental and resulting standard errors might be underestimated! Use this if for some strata the whole population is in the sample! 
-#' 
-#' @details For specifying multistage sampling designs the column names in \code{strata},\code{cluster} and \code{fpc} need to seperated by ">".\cr
+#' @param new.method logical, if TRUE bootstrap replicates will never be negative even if in some strata the whole population is in the sample. WARNING: This is still experimental and resulting standard errors might be underestimated! Use this if for some strata the whole population is in the sample!
+#'
+#' @details For specifying multistage sampling designs the column names in `strata`,`cluster` and `fpc` need to seperated by ">".\cr
 #' For multistage sampling the strings are read from left to right meaning that the column name before the first ">" is taken as the column for stratification/clustering/number of PSUs at the first and the column after the last ">" is taken as the column for stratification/clustering/number of PSUs at the last stage.
-#' If for some stages the sample was not stratified or clustered one must specify this by "1" or "I", e.g. \code{strata=c("strata1>I>strata3")} if there was no stratification at the second stage or \code{cluster=c("cluster1>cluster2>I")} if there were no clusters at the last stage.\cr
+#' If for some stages the sample was not stratified or clustered one must specify this by "1" or "I", e.g. `strata=c("strata1>I>strata3")` if there was no stratification at the second stage or `cluster=c("cluster1>cluster2>I")` if there were no clusters at the last stage.\cr
 #' The number of PSUs at each stage is not calculated internally and must be specified for any sampling design.
 #' For single stage sampling using stratification this can usually be done by adding over all sample weights of each PSU by each strata-code.\cr
 #' Spaces in each of the strings will be removed, so if column names contain spaces they should be renamed before calling this procedure!
 #'
-#' @return returns the complete data set including the bootstrap replicates or just the bootstrap replicates, depending on \code{return.value="data"} or \code{return.value="replicates"} respectively.
+#' @return returns the complete data set including the bootstrap replicates or just the bootstrap replicates, depending on `return.value="data"` or `return.value="replicates"` respectively.
 #' @export rescaled.bootstrap
 #'
 #' @references Preston, J. (2009). Rescaled bootstrap for stratified multistage sampling. Survey Methodology. 35. 227-234.
@@ -49,28 +49,28 @@
 #'
 #' eusilc.bootstrap <- rescaled.bootstrap(eusilc,REP=100,strata=c("new_strata"),
 #'                                        cluster="db030",fpc="N.households")
-#' 
+#'
 #'
 
 
 rescaled.bootstrap <- function(dat,REP=1000,strata="DB050>1",cluster="DB060>DB030",fpc="N.cluster>N.households",
                                single.PSU=c("merge","mean"), return.value=c("data","replicates"),check.input=TRUE,new.method=FALSE){
-  
-  InitialOrder <- N <- SINGLE_BOOT_FLAG <- SINGLE_BOOT_FLAG_FINAL <- f <- n_prev <- n_draw_prev <- sum_prev <- n_draw <- NULL 
-  
+
+  InitialOrder <- N <- SINGLE_BOOT_FLAG <- SINGLE_BOOT_FLAG_FINAL <- f <- n_prev <- n_draw_prev <- sum_prev <- n_draw <- NULL
+
   # prepare input
   input <- c(strata,cluster,fpc)
   input <- gsub("\\s","",input)
   input <- strsplit(input,">")
-  
+
   strata <- input[[1]]
   cluster <- input[[2]]
   fpc <- input[[3]]
-  
+
   single.PSU <- single.PSU[1]
   return.value <- return.value[1]
-  
-  
+
+
   if(!is.logical(check.input)){
     stop("check.input can only be logical")
   }
@@ -95,7 +95,7 @@ rescaled.bootstrap <- function(dat,REP=1000,strata="DB050>1",cluster="DB060>DB03
         stop("REP cannot have a decimal part")
       }
     }
-    
+
     # check design variables
     if(length(unique(lapply(input,length)))>1){
       stop("strata, cluster, and fpc need to have the same number of arguments separated with '>'")
@@ -106,17 +106,17 @@ rescaled.bootstrap <- function(dat,REP=1000,strata="DB050>1",cluster="DB060>DB03
     if(length(check.values)>0){
       stop("dat does not contain the column(s): ",check.values)
     }
-    
+
     # check if there are missings in fpc
     if(any(is.na(dat[,.SD,.SDcols=c(fpc)]))){
       stop("missing values not allowed in fpc")
     }
-    
+
     # check return.value
     if(!return.value%in%c("data","replicates")){
       stop("return.value can only take the values 'data' or 'replicates'")
     }
-    
+
     # check single.PSU
     if(is.null(single.PSU)){
       warning("single.PSU was not set to either 'merge' or 'mean'!\n Bootstrap replicates for single PSUs cases will be missing!")
@@ -133,15 +133,15 @@ rescaled.bootstrap <- function(dat,REP=1000,strata="DB050>1",cluster="DB060>DB03
   if(length(overwrite.names)>0){
     overwrite.names.new <- paste0("ORIGINAL_",overwrite.names)
     setnames(dat,overwrite.names,overwrite.names.new)
-    
+
     strata[strata%in%overwrite.names] <- overwrite.names.new[strata%in%overwrite.names]
     cluster[cluster%in%overwrite.names] <- overwrite.names.new[cluster%in%overwrite.names]
     fpc[fpc%in%overwrite.names] <- overwrite.names.new[fpc%in%overwrite.names]
   }
-  
+
   # set index for data to return dat in correct order
   dat[,InitialOrder:=.I]
-  
+
   # calculate bootstrap replicates
   stages <- length(strata)
   n <- nrow(dat)
@@ -150,9 +150,9 @@ rescaled.bootstrap <- function(dat,REP=1000,strata="DB050>1",cluster="DB060>DB03
   N.calc <- matrix(0,nrow=n,ncol=stages)
   n_draw.calc <- matrix(0,nrow=n,ncol=stages)
   delta.calc <- array(0,dim=c(n,stages,REP))
-  
+
   for(i in 1:stages){
-    
+
     # define by.val
     if(i>1){
       by.val <- c(strata[1:(i-1)],cluster[1:(i-1)],strata[i])
@@ -160,7 +160,7 @@ rescaled.bootstrap <- function(dat,REP=1000,strata="DB050>1",cluster="DB060>DB03
       by.val <- strata[1:i]
     }
     by.val <- by.val[!by.val%in%c("1","I")]
-    
+
     # define cluster value
     clust.val <- cluster[i]
     if(clust.val%in%c("1","I")){
@@ -173,7 +173,7 @@ rescaled.bootstrap <- function(dat,REP=1000,strata="DB050>1",cluster="DB060>DB03
     if(nrow(check.fpc)>0){
       stop("values in ", fpc[i]," do vary in some strata-cluster combinations at stage ",i)
     }
-    
+
     singles <- dt.eval("dat[,sum(!duplicated(",clust.val,")),by=c(by.val)][V1==1]")
     if(nrow(singles)>0){
       # if singel.PSU=="merge" change the coding of the current stage of the single PSU
@@ -190,13 +190,13 @@ rescaled.bootstrap <- function(dat,REP=1000,strata="DB050>1",cluster="DB060>DB03
           dt.eval("dat[,",paste0(tail(by.val,1),"_ORIGINALSINGLES"),":=",tail(by.val,1),"]")
           dt.eval("dat[,",paste0(fpc[i],"_ORIGINALSINGLES"),":=",fpc[i],"]")
         }
-        
+
         setkeyv(dat,higher.stages)
         next.PSU <- dt.eval("dat[singles,.(N=sum(!duplicated(",clust.val,"))),by=c(by.val)]")
-        
+
         new.var <- paste0(tail(by.val,1),"_NEWVAR")
         dt.eval("next.PSU[,c(new.var):=next_minimum(N,",tail(by.val,1),"),by=c(higher.stages)]")
-        
+
         next.PSU <- next.PSU[N==1]
         dat <- merge(dat,next.PSU[,mget(c(by.val,new.var))],by=c(by.val),all.x=TRUE)
         # sum over margins
@@ -206,12 +206,12 @@ rescaled.bootstrap <- function(dat,REP=1000,strata="DB050>1",cluster="DB060>DB03
         dt.eval("dat[,",fpc[i],":=",fpc[i],"[is.na(",new.var,")][1],by=c(by.val)]")
         dt.eval("dat[!is.na(",new.var,"),",fpc[i],":=",fpc[i],"+",paste0(fpc[i],"_ADD"),"]")
         dt.eval("dat[,",fpc[i],":=max(",fpc[i],"),by=c(by.val)]")
-        
+
         dat[,c(new.var,paste0(fpc[i],"_ADD")):=NULL]
       }else if(single.PSU=="mean"){
         # if single.PSU="mean" flag the observation as well as the all the observations in the higher group
         singles[,SINGLE_BOOT_FLAG:=paste(higher.stages,.GRP,sep="-"),by=c(higher.stages)]
-        
+
         dat <- merge(dat,singles,by=c(higher.stages),all.x=TRUE)
         if(!"SINGLE_BOOT_FLAG_FINAL"%in%colnames(dat)){
           dat[,SINGLE_BOOT_FLAG_FINAL:=SINGLE_BOOT_FLAG]
@@ -219,13 +219,13 @@ rescaled.bootstrap <- function(dat,REP=1000,strata="DB050>1",cluster="DB060>DB03
           dat[is.na(SINGLE_BOOT_FLAG_FINAL),SINGLE_BOOT_FLAG_FINAL:=SINGLE_BOOT_FLAG]
         }
         dat[,SINGLE_BOOT_FLAG:=NULL]
-        
+
       }else{
         warning("Single PSUs detected at the following stages:")
         print(dt.eval("dat[,sum(!duplicated(",clust.val,")),by=c(by.val)][V1==1]"))
       }
     }
-    
+
     # get Stage
     if(i==1){
       dati <- dt.eval("dat[,.(N=",fpc[i],"[1],",clust.val,"=unique(",clust.val,"),f=1,n_prev=1,n_draw_prev=1,sum_prev=1),by=list(",paste(by.val,collapse=","),")]")
@@ -236,7 +236,7 @@ rescaled.bootstrap <- function(dat,REP=1000,strata="DB050>1",cluster="DB060>DB03
       dat[,n_draw_prev:=NULL]
       dat[,sum_prev:=NULL]
     }
-    
+
     deltai <- paste0("delta_",i,"_",1:REP)
     dati[,n:=.N,by=c(by.val)]
     # determin number of psu to be drawn
@@ -248,33 +248,33 @@ rescaled.bootstrap <- function(dat,REP=1000,strata="DB050>1",cluster="DB060>DB03
     }
     # do bootstrap for i-th stage
     dati[,c(deltai):=as.data.table(replicate(REP,draw.without.replacement(n[1],n_draw[1]),simplify = FALSE)),by=c(by.val)]
-    
+
     # merge with data
     dat <- merge(dat,dati,by=c(by.val,clust.val))
-    
+
     # extract information from data.table and remove again from data table (less memory intensive)
     # only matrices and arrays needed for final calculation
     n.calc[,i] <- dat[,n]
     N.calc[,i] <- dat[,N]
     n_draw.calc[,i] <- dat[,n_draw]
     delta.calc[,i,] <- as.matrix(dat[,mget(deltai)])
-    
+
     dat[,sum_prev:=sum_prev + (sqrt(n_draw*f*(1-n/N)/(n-n_draw)) * sqrt(n_prev/n_draw_prev) * (n/n_draw-1))]
     dat[,f:=n/N*f]
     dat[,n_prev:=n*n_prev]
     dat[,n_draw_prev:=n_draw*n_draw_prev]
-    
+
     dat[,c("n","N",deltai,"n_draw"):=NULL]
-    
+
   }
-  
+
   bootRep <- paste0("bootRep",1:REP)
   dat[,c(bootRep):=as.data.table(calc.replicate(n=n.calc,N=N.calc,n_draw=n_draw.calc,delta=delta.calc))]
-  
+
   if(single.PSU=="mean"){
     dat[!is.na(SINGLE_BOOT_FLAG_FINAL),c(bootRep):=lapply(.SD,function(z){mean(z,na.rm=TRUE)}),by=SINGLE_BOOT_FLAG_FINAL,.SDcols=c(bootRep)]
   }
-  
+
   setkey(dat,InitialOrder)
   if(return.value=="data"){
     # get original col values
@@ -299,7 +299,7 @@ rescaled.bootstrap <- function(dat,REP=1000,strata="DB050>1",cluster="DB060>DB03
 }
 
 select.nstar <- function(n,N,f,n_prev,n_draw_prev,lambda_prev,sum_prev=NULL,new.method){
-  
+
   if(!is.null(sum_prev)){
     n_draw <- (sum_prev)^2/((1-(n/N))*n_prev*f+(sum_prev)^2)*n
     n_draw <- floor(n_draw)
