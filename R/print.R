df00162ace93741108e882679b6ca36ca7115e9d
Gregor de Cillia <de.cillia.gregor@gmail.com>
2018-09-28 18:10:43 +0200

    more markdown documentation

--- R/print.R
+++ R/print.R
@@ -1,10 +1,10 @@
 #' @title Print function for surveysd objects
 #'
 #' @description
-#' Prints the results of a call to \code{\link[surveysd]{calc.stError}}. Shows used variables and function, number of point estiamtes
+#' Prints the results of a call to [calc.stError]. Shows used variables and function, number of point estiamtes
 #' as well as properties of the results.
 #'
-#' @param x an object of class \code{'surveysd'}
+#' @param x an object of class `'surveysd'`
 #' @param ... additonal parameters
 #'
 #' @export
