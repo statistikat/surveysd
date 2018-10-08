091f87ee48fe3196053d496994bacdc06e396d8c
Gregor de Cillia <de.cillia.gregor@gmail.com>
2018-10-08 16:10:14 +0200

    Document and export `demo.eusilc()`
    Create documentation for the testdata used
    throughout the documentation of this package.
    
    Add a parameter "perttyNames" which renames the
    variables and some of the facor-levels in order
    to make the documentation easier to understand
    in upcoming versions

---
+++ R/demo.eusilc.R
@@ -0,0 +1,74 @@
+#' helpfunction to generate multiple years of eusilc data
+#'
+#' Create a dummy dataset to be used for demonstrating the functionalities of the `surveysd` package
+#' based on [leaken::eusilc]. Please refer to the documentation page of the original data for
+#' details about the variables.
+#'
+#' @param n Number of years to generate. Should be at least 1
+#' @param prettyNames Create easy-to-read names for certain variables. Recommended for demonstration
+#'                    purposes. Otherwise, use the original codes documented in [leaken::eusilc].
+#'
+#' @details
+#' If `prettyNames` is `TRUE`, the following variables will be available in an easy-to-read manner.
+#'
+#' * `hid` Household id. Consistent with respect to the reference period (`year`)
+#' * `hsize` Size of the household. derived from `hid` and `period`
+#' * `region` Federal state of austria where the household is located
+#' * `pid` Personal id. Consistent with respect to the reference period (`year`)
+#' * `age` Age-class of the respondent
+#' * `gender` A persons gender (`"male"`, `"Female"`)
+#' * `ecoStat` Ecnomic status (`"part time"`, `"full time"`, `"unemployed"`, ...)
+#' * `citizenship` Citizenship (`"AT"`, `"EU"`, `"other"`)
+#' * `pWeight` Personal sample weight inside the reference period
+#' * `year`. Simulated reference period
+#' * `povertyRisk`. Logical variable determining whether a respondent is at risk of poverty
+#'
+#' @examples
+#' demo.eusilc(n = 1, prettyNames = TRUE)[, c(1:8, 26, 28:30)]
+#' @export
+demo.eusilc <- function(n = 8, prettyNames = FALSE) {
+
+  db030 <- rb030 <- povmd60 <- eqincome <- db090 <- eqIncome <- age <- hsize <- . <- NULL
+
+  data("eusilc", package = "laeken", envir = environment())
+  setDT(eusilc)
+  # generate yearly data for y years
+  # 25% drop out from 1 year to the other
+  eusilc[,year := 2010]
+  eusilc.i <- copy(eusilc)
+  nsamp <- round(eusilc[, uniqueN(db030)]*.25)
+  hhincome <- eusilc[!duplicated(db030)][["eqIncome"]]
+  nextIDs <- (1:nsamp) + eusilc[ ,max(db030)]
+  if (n > 1)
+    for(i in 1:(n-1)) {
+      eusilc.i[db030%in%sample(unique(eusilc.i$db030), nsamp),
+               c("db030","eqIncome"):=.(nextIDs[.GRP], sample(hhincome, .N)),
+               by = db030]
+      eusilc.i[,year := year + 1]
+      eusilc <- rbind(eusilc, eusilc.i)
+      nextIDs <- (1:nsamp) + eusilc[, max(db030)]
+    }
+
+  eusilc[, rb030 := as.integer(paste0(db030, "0", 1:.N)), by = list(year, db030)]
+  eusilc[, povmd60 := as.numeric(eqIncome < .6 * laeken::weightedMedian(eqIncome, w = db090)), by = year]
+  eusilc[, age := cut(age, c(-Inf, 16, 25, 45, 65, Inf))]
+  eusilc[, hsize := cut(hsize, c(0:5, Inf))]
+
+  if (prettyNames) {
+    data.table::setnames(eusilc, "db030", "hid")
+    data.table::setnames(eusilc, "db040", "region")
+    data.table::setnames(eusilc, "rb030", "pid")
+    data.table::setnames(eusilc, "rb090", "gender")
+    data.table::setnames(eusilc, "pb220a", "citizenship")
+    data.table::setnames(eusilc, "pl030", "ecoStat")
+    eusilc[, ecoStat := dplyr::recode(
+      ecoStat, `1` = "full time", `2` = "part time", `3` = "unemployed",
+      `4` = "education", `5` = "retired", `6` = "disabled", `7` = "domestic")]
+    data.table::setnames(eusilc, "rb050", "pWeight")
+    #data.table::setnames(eusilc, "db090", "hWeight")
+    data.table::setnames(eusilc, "povmd60", "povertyRisk")
+    eusilc[, povertyRisk := as.logical(povertyRisk)]
+  }
+
+  return(eusilc)
+}
--- R/helpers.R
+++ R/helpers.R
@@ -1,7 +1,7 @@
 ##############################################################
 # Helper-Functions
 #
-#' @import ggplot2 
+#' @import ggplot2
 #' @import simPop data.table Rcpp
 #' @importFrom "graphics" "plot"
 #' @importFrom "stats" "as.formula" "na.omit" "quantile" "sd"
@@ -64,7 +64,7 @@ povmd <- function(x,w){
 
 # helpfunction for quantile calcultion with missings
 quantileNA <- function(x,probs,p.names,np=length(probs)){
-  
+
   if(any(is.na(x))){
     out <- rep(NA_real_,np)
   }else{
@@ -74,45 +74,13 @@ quantileNA <- function(x,probs,p.names,np=length(probs)){
   return(out)
 }
 
-# helpfunction to generate multiple years of eusilc data
-demo.eusilc <- function(y=7){
-  
-  db030 <- rb030 <- povmd60 <- eqincome <- db090 <- eqIncome <- age <- hsize <- . <- NULL
-  
-  data("eusilc",package="laeken",envir = environment())
-  setDT(eusilc)
-  # generate yearly data for y years
-  # 25% drop out from 1 year to the other
-  eusilc[,year:=2010]
-  eusilc.i <- copy(eusilc)
-  nsamp <- round(eusilc[,uniqueN(db030)]*.25)
-  hhincome <- eusilc[!duplicated(db030)][["eqIncome"]]
-  nextIDs <- (1:nsamp)+eusilc[,max(db030)]
-  for(i in 1:y){
-    eusilc.i[db030%in%sample(unique(eusilc.i$db030),nsamp),
-             c("db030","eqIncome"):=.(nextIDs[.GRP],sample(hhincome,.N)),
-             by=db030]
-    eusilc.i[,year:=year+1]
-    eusilc <- rbind(eusilc,eusilc.i)
-    nextIDs <- (1:nsamp)+eusilc[,max(db030)]
-  }
-  
-  eusilc[,rb030:=as.integer(paste0(db030,"0",1:.N)),by=list(year,db030)]
-  eusilc[,povmd60:=as.numeric(eqIncome<.6*laeken::weightedMedian(eqIncome,w=db090)),by=year]
-  eusilc[,age:=cut(age,c(-Inf,16,25,45,65,Inf))]
-  eusilc[,hsize:=cut(hsize,c(0:5,Inf))]
-  
-  return(eusilc)
-}
-
-randomInsert <- function(x,y,n=20){
-  if(length(x)<20|length(y)<20){
+randomInsert <- function(x, y, n = 20){
+  if(length(x) < 20 | length(y) < 20){
     stop("n must be smaller than length(x) and length(y)")
   }
 
-  x.indices <- sample(length(x),n)
-  y.values <- sample(y,n)
+  x.indices <- sample(length(x), n)
+  y.values <- sample(y, n)
   x[x.indices] <- y.values
   return(x)
 }
-
