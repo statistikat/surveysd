5f3734ba11db5cc3687d69f6b9208ecd80dfc9dd
alexkowa <alex@kowarik.net>
2020-06-30 13:31:02 +0200

    kishFactor help a little improved

--- R/kishFactor.R
+++ R/kishFactor.R
@@ -1,13 +1,17 @@
 #'
 #' Kish Factor
 #'
-#' Compute the kish factor for a specific weight vector
+#' Compute the design effect due to unequal weighting.
 #'
 #' @name kishFactor
 #' @param w a numeric vector with weights
 #' @return The function will return the the kish factor
 #' @author Alexander Kowarik
 #' @export kishFactor
+#' @details The factor is computed acording to 'Weighting for Unequal P_i', Leslie Kish, Journal of Official Statistics, Vol. 8. No. 2, 1992
+#' \deqn{ deff = \sqrt n \sum_j w_j^2 / (\sum_j w_j)^2}
+
+#'
 #' @examples
 #' kishFactor(rep(1,10))
 #' kishFactor(rlnorm(10))
