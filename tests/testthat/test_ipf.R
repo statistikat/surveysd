02fad6cb8ca353c43e964c81c75dc20eb30604c3
alexkowa <alex@kowarik.net>
2022-10-05 11:10:46 +0200

    summary.ipf

--- tests/testthat/test_ipf.R
+++ tests/testthat/test_ipf.R
@@ -195,3 +195,14 @@ test_that("ipf works as expected with minMaxTrim P and HH", {
   expect_true(err < .01)
   expect_true(all(calibweights2$calibWeight<=870)&&all(calibweights2$calibWeight>=340))
 })
+
+test_that("summary.ipf", {
+  # with array epsP1, base weights and bound
+  calibweights2 <- ipf(
+    eusilc, hid = "hid", conP = list(conP1, conP2), conH = list(conH1),
+    epsP = 1e-06, epsH = list(epsH1), w = "baseWeight", bound = 4,
+    verbose = FALSE, maxIter = 200, minMaxTrim = c(340,870))
+  summary.ipf(calibweights2)
+
+})
+
